8722c9212663cf76a5352a17452220cbdd2b6508
aekuryr <andres.kury@salud.gob.sv>
2025-07-17 14:59:38 -0600

    Update app.py

--- app.py
+++ app.py
@@ -69,13 +69,20 @@ def calcular_indicadores_ce(df):
 
     # --- 3. CE sin notificación
     ce_agend_notif = ce_total[
-        (ce_total['area_remision'] == 'consulta') &
-        (ce_total['fecha_cita_destino'].notna())
-    ]
-    num_sin_notif = len(ce_agend_notif[ce_agend_notif['paciente_notificado'] == 'no'])
-    den_sin_notif = len(ce_agend_notif)
-    pct_sin_notif = (num_sin_notif / den_sin_notif * 100) if den_sin_notif else 0
+    (ce_total['area_remision'] == 'consulta') &
+    (ce_total['fecha_cita_destino'].notna())
+]
 
+num_sin_notif = len(
+    ce_agend_notif[
+        ce_agend_notif['paciente_notificado'].isna() |
+        (ce_agend_notif['paciente_notificado'].astype(str).str.strip() == '')
+    ]
+)
+
+den_sin_notif = len(ce_agend_notif)
+
+pct_sin_notif = (num_sin_notif / den_sin_notif * 100) if den_sin_notif else 0
     # --- 4. CE efectivas
     df_pasadas = df[df['fecha_cita_destino'] < hoy]
     ce_nr_pas = df_pasadas[df_pasadas['referencia_rechazada'] != 'si']
